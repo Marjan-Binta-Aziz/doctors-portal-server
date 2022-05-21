585f599948edc4f2566ca622e4419e02e9836ed1
marjan <74914575+Marjan-Binta-Aziz@users.noreply.github.com>
2022-05-21 23:50:47 +0600

    ifntAdmin_notAllowtoMakeAnotherAdmin

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken')
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -13,6 +14,22 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yllpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+function verifyJWT(req, res, next){
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({message: 'Unauthorized Access'})
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if (err) {
+            return res.status(403).send({message: 'Forbidden Access'})
+        }
+        req.decoded = decoded;
+        next();
+    });
+
+}
 async function run() {
     try {
         await client.connect();
@@ -20,16 +37,42 @@ async function run() {
         const bookingCollection = client.db("doctor's_portal").collection("bookings");
         const userCollection = client.db("doctor's_portal").collection("users");
 
+
+        app.get('/all-user',verifyJWT, async(req, res)=>{
+            const allUser = await userCollection.find().toArray();
+            res.send(allUser);
+        });
+
+        app.put('/user/admin/:email',verifyJWT, async(req, res)=> {
+            const email = req.params.email;
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({email: requester});
+            if (requesterAccount.role === 'admin') {
+            const filter = {email: email};
+            const updatedDoc = {
+                $set: {role: 'admin'},
+            };
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            console.log(email);
+            res.send(result);
+            }else{
+                res.status(403).send('Forbidden')
+            }
+        
+        });
+
         app.put('/user/:email', async(req, res)=> {
             const email = req.params.email;
+            const user = req.body;
             const filter = {email: email};
             const options = {upsert: true};
             const updatedDoc = {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-        })
+            const token = jwt.sign({email: email}, process.env.ACCESS_TOKEN , {expiresIn: '1h'});
+            res.send({result, token});
+        });
 
 
         app.get('/appointment', async(req, res)=> {
@@ -67,17 +110,23 @@ async function run() {
 
 
 
-        app.get('/booking', async(req,res)=>{
-            const patientEmail = req.query.patientEmail;
-            const query = {patientEmail : patientEmail};
+        app.get('/booking', verifyJWT ,async(req,res)=>{
+            const email = req.query.email;
+            const decodedEmail = req.decoded.email;
+            if (email === decodedEmail) {
+            
+            const query = {email : email};
+            const authorization = req.headers.authorization;
             const booking = await bookingCollection.find(query).toArray();
             res.send(booking);
+            }
+            
 
         })
 
         app.post('/booking', async(req, res)=>{
             const booking = req.body;
-            const query = { treatment: booking.treatment, date:booking.date, patientEmail: booking.patientEmail}
+            const query = { treatment: booking.treatment, date:booking.date, email: booking.email}
             const exists = await bookingCollection.findOne(query);
             if (exists) {
                 return res.send({success: false, booking: exists})
